083a5a1a56c1bc3920ee663de359e43338599fd2
boter <boter@MSI>
2021-07-12 13:17:00 +0200

    variables

--- 001Basico/src/com/arquitecturajava/HolaMundo.java
+++ 001Basico/src/com/arquitecturajava/HolaMundo.java
@@ -1,5 +1,7 @@
 package com.arquitecturajava;
 
+import java.nio.file.ReadOnlyFileSystemException;
+
 public class HolaMundo {
 
 	public static void main(String[] args) {
@@ -8,7 +10,26 @@ public class HolaMundo {
 		System.out.println("hola2");
 		System.out.println("hola marcelo 2");
 		System.out.println("hola marcelo 3");
-
+		
+		byte numero=5;
+		short numero2=700;
+		int numero3=2000000;
+		long numero4=20000000;
+		
+		System.out.println(numero);
+		System.out.println(numero2);
+		System.out.println(numero3);
+		System.out.println(numero4);
+		
+		float numero5=2000.2056f;
+		double numero6=2000.456;
+		System.out.println(numero5);
+		System.out.println(numero6);
+		
+		char letra='a';
+		System.out.println(letra);
+		boolean cierto=true;
+		System.out.println(cierto);
 	}
 
 }
