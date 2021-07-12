ff5d7ecb2626d14ca2b810e0fcc456d31ea671b8
Ruben <Ruben@DESKTOP-T4NQLMI.home>
2021-07-12 14:24:30 +0200

    mas clases

---
+++ 001Basico/src/com/arquitecturajava/HolaMundo7.java
@@ -0,0 +1,22 @@
+package com.arquitecturajava;
+
+
+
+public class HolaMundo7 {
+
+	public static void main(String[] args) {
+		
+		int numero=2;
+		
+		if(numero==2 || numero==4 || numero == 6 || numero == 8) {
+			System.out.println("el número es par");
+		}
+		
+		if (numero % 2 == 0) {
+			System.out.println("el número es par");
+		}
+		
+		//operador ternario
+		System.out.println((numero > 5 ? "has aprobado" : "has suspendido"));
+	}
+}
