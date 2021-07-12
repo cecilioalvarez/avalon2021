969667a00a10ddbf7ff334d3561c912f33670631
miguel <miguel.perez@juanxxiii.net>
2021-07-12 14:16:36 +0200

    HM6

---
+++ 001Basico/src/com/arquitecturajava/HolaMundo6.java
@@ -0,0 +1,22 @@
+package com.arquitecturajava;
+
+import java.util.Scanner;
+
+public class HolaMundo6 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num1 = 2;
+		int num2 = 3;
+		int num4 = 5;
+		int num6 = 5;
+		
+		//== es Comparador de Comparación y != es Distinto
+		System.out.println(num1=num2);
+		System.out.println(num1==num2);
+		System.out.println(num4!=num6);
+		System.out.println(num4==num6);
+		
+		
+	}
+}
