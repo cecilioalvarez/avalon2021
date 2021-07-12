dbf1a92587121f2640c8d8df56773938fe4ca753
MARIO <MARIO@DESKTOP-A888AC8>
2021-07-12 13:06:10 +0200

    ejercicio1

--- 001Basico/src/com/arquitecturajava/HolaMundo.java
+++ 001Basico/src/com/arquitecturajava/HolaMundo.java
@@ -8,7 +8,14 @@ public class HolaMundo {
 		System.out.println("hola2");
 		System.out.println("Hola Mario");
 		
-
+		byte numero=5;
+		short numero2=700;
+		int numero3=2000000;
+		long numero4=200000000;
+		System.out.println(numero);
+		System.out.println(numero2);
+		System.out.println(numero3);
+		System.out.println(numero4);
 	}
 
 }
