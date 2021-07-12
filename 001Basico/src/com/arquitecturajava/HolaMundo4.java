87a4efbe924b6e9648d3f95374078ae26a3687bb
cecilioAlvarez <cmac@arquitecturajava.com>
2021-07-12 13:46:55 +0200

    nuevos operadores logicos

--- 001Basico/src/com/arquitecturajava/HolaMundo4.java
+++ 001Basico/src/com/arquitecturajava/HolaMundo4.java
@@ -1,5 +1,6 @@
 package com.arquitecturajava;
 
+import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class HolaMundo4 {
@@ -8,8 +9,12 @@ public class HolaMundo4 {
 		
 		double nota1=4.2;
 		double nota2=4.3;
+		BigDecimal numero3= new BigDecimal(100.2434567888888);
+		BigDecimal numero4= new BigDecimal(100.2423456777777);
+		BigDecimal numero5=numero4.subtract(numero3);
+		
 		System.out.println(nota1+nota2);
-		System.out.println(nota1-nota2);
+		System.out.println(numero5.doubleValue());
 		System.out.println(nota1/nota2);
 		System.out.println(nota1*nota2);
 		
