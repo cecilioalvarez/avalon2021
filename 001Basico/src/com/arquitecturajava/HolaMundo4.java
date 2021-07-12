f11347a4755ba4f4d40b99e26f8d6407a182fb97
cecilioAlvarez <cmac@arquitecturajava.com>
2021-07-12 13:33:51 +0200

    suma resta etc

---
+++ 001Basico/src/com/arquitecturajava/HolaMundo4.java
@@ -0,0 +1,19 @@
+package com.arquitecturajava;
+
+import java.util.Scanner;
+
+public class HolaMundo4 {
+
+	public static void main(String[] args) {
+		
+		double nota1=4.2;
+		double nota2=4.3;
+		System.out.println(nota1+nota2);
+		System.out.println(nota1-nota2);
+		System.out.println(nota1/nota2);
+		System.out.println(nota1*nota2);
+		
+		
+	}
+	
+}
