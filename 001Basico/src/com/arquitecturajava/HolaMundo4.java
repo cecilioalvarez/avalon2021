1855e319c418678bd4eb973435a1db8135249dc9
Ruben <Ruben@DESKTOP-T4NQLMI.home>
2021-07-12 13:38:56 +0200

    nuevas clases

---
+++ 001Basico/src/com/arquitecturajava/HolaMundo4.java
@@ -0,0 +1,15 @@
+package com.arquitecturajava;
+
+public class HolaMundo4 {
+
+	public static void main(String[] args) {
+		
+		double nota1 = 4.2;
+		double nota2 = 4.3;
+		System.out.println(nota1+nota2);
+		System.out.println(nota1-nota2);
+		System.out.println(nota1/nota2);
+		System.out.println(nota1*nota2);
+	}
+
+}
