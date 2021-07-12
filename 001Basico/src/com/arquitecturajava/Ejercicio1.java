edcacfbace8416ebf66e7748498db5d8c5915ca1
Molocale <molocale@gmail.com>
2021-07-12 14:07:16 +0200

    Ejercicio 1

---
+++ 001Basico/src/com/arquitecturajava/Ejercicio1.java
@@ -0,0 +1,24 @@
+package com.arquitecturajava;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce un numero");
+		int numero = sc.nextInt();
+		Scanner sc2 = new Scanner(System.in);
+		System.out.println("Introduce otro numero");
+		int numero2 = sc2.nextInt();
+		if (numero > numero2) {
+			System.out.println("El numero mayor es: " + numero);
+
+			
+		} else {
+			System.out.println(numero2);
+
+		}
+	
+	}
+
+}
