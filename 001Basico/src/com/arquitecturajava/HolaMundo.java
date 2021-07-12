826bc68e3871a518d015281bc8d33a8e42f11872
Ruben <Ruben@DESKTOP-T4NQLMI.home>
2021-07-12 13:06:42 +0200

    nuevos tipos de variables añadidos

--- 001Basico/src/com/arquitecturajava/HolaMundo.java
+++ 001Basico/src/com/arquitecturajava/HolaMundo.java
@@ -8,6 +8,16 @@ public class HolaMundo {
 		System.out.println("hola2");
 		System.out.println("hola Ruben");
 
+		// los tipos de variables son o basicos o objetos
+		
+		byte numero=5;
+		short numero2=700;
+		int numero3=2000000;
+		long numero4=20000000;
+		System.out.println(numero);
+		System.out.println(numero2);
+		System.out.println(numero3);
+		System.out.println(numero4);
 	}
 
 }
