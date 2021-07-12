de3eeaf17c6e8cc7dbebfdcf80f86540cc9cfee6
Francisco Villa <francisco.villa.gonzalez@gmail.com>
2021-07-12 13:10:23 +0200

    variables basicas

--- 001Basico/src/com/arquitecturajava/HolaMundo.java
+++ 001Basico/src/com/arquitecturajava/HolaMundo.java
@@ -9,6 +9,28 @@ public class HolaMundo {
 		System.out.println("hola francisco");
 		System.out.println("hola francisco2");
 		
+		
+		byte numero=5;
+		short numero2=700;
+		int numero3=200000;
+		long numero4=200000000;
+		float numero5=2000.20f;
+		double numero6=2000.456;
+		char letra='a';
+		boolean cierto=true;
+		
+		
+		System.out.println(numero);
+		System.out.println(numero2);
+		System.out.println(numero3);
+		System.out.println(numero4);
+		System.out.println(numero5);
+		System.out.println(numero6);
+		System.out.println(letra);
+		System.out.println(cierto);
+		
+		
+		
 
 	}
 
