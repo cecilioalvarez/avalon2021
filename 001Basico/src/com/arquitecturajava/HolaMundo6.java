ecdb8ed843297de38719762d84f139c92fbf2ca8
Ruben <Ruben@DESKTOP-T4NQLMI.home>
2021-07-12 14:07:43 +0200

    ejercicio realizado

---
+++ 001Basico/src/com/arquitecturajava/HolaMundo6.java
@@ -0,0 +1,23 @@
+package com.arquitecturajava;
+
+import java.util.Scanner;
+
+public class HolaMundo6 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Escribe el primer número");
+		int numero1 = sc.nextInt();
+		System.out.println("Escribe el segundo número");
+		int numero2 = sc.nextInt();
+		
+		if(numero1>numero2) {
+			System.out.println(numero1);
+			System.out.println(numero2);
+		} else {
+			System.out.println(numero2);
+			System.out.println(numero1);
+		}
+	}
+}
