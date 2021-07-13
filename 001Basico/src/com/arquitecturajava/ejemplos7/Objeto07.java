6528549a53bdb6289bc705c09286372932d42861
Ruben <Ruben@DESKTOP-T4NQLMI.home>
2021-07-13 13:47:44 +0200

    arreglado problema caja

--- 001Basico/src/com/arquitecturajava/ejemplos7/Objeto07.java
+++ 001Basico/src/com/arquitecturajava/ejemplos7/Objeto07.java
@@ -5,7 +5,11 @@ public class Objeto07 {
 	public static void main(String[] args) {
 		Caja c = new Caja(20,30,40);
 		System.out.println(c.calcularVolumen());
-		
+		Caja contenedor= c.getCajaVolumenHuecoMinimo();
+		System.out.println(contenedor.getAncho());
+		System.out.println(contenedor.getLargo());
+		System.out.println(contenedor.getAlto());
+		System.out.println(contenedor.calcularVolumen());
 	}
 
 }
