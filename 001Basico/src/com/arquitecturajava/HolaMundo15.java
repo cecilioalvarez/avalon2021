bc5aeaf23c660ee47e1336482d27c8e5cee66846
Germán Zunzunegui <gerzunzu@gmail.com>
2021-07-13 10:01:00 +0200

    N - Cambios hasta ejercicios con arrays.

---
+++ 001Basico/src/com/arquitecturajava/HolaMundo15.java
@@ -0,0 +1,30 @@
+package com.arquitecturajava;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+class HolaMundo15 {
+    
+    private static int askForInteger() {
+        boolean valid = false;
+        int number = 0;
+        do {
+            System.out.print("Introduce un número entero:\t");
+            try {
+                number = new Scanner(System.in).nextInt();
+                valid = true;
+            } catch (InputMismatchException ime_ex) {
+                System.err.println("Error en la entrada de datos.");
+            }
+        } while (!valid);
+        return number;
+    }
+    
+    private static double sum(int n1, int n2) {
+        return n1 + n2;
+    }
+
+    public static void main(String[] args) {
+        HolaMundo15.sum(HolaMundo15.askForInteger(), HolaMundo15.askForInteger());
+    }
+}
---
+++ 001Basico/src/com/arquitecturajava/HolaMundo16.java
@@ -0,0 +1,16 @@
+package com.arquitecturajava;
+
+public class HolaMundo16 {
+    
+    private static double bills[] = new double[3];
+    
+    public static void main(String[] args) {
+        bills[0] = 135.85;
+        bills[1] = 298.99;
+        bills[2] = 87.00;
+        
+        for (int i = 0; i < bills.length; i++) {
+            System.out.println(bills[i]);
+        }
+    }
+}
