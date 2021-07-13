d4681ebf208a86c0b4b0d95a23ed68d1f75b9d22
miguel <miguel.perez@juanxxiii.net>
2021-07-13 09:16:03 +0200

    HM 10 y 11

---
+++ 001Basico/src/com/arquitecturajava/HolaMundo10.java
@@ -0,0 +1,11 @@
+package com.arquitecturajava;
+
+public class HolaMundo10 {
+	public static void main(String[] args) {
+		
+		for(int i =0;i<=10;i++) {
+			System.out.println(7*i);
+		}
+		
+	}
+}
---
+++ 001Basico/src/com/arquitecturajava/HolaMundo11.java
@@ -0,0 +1,14 @@
+package com.arquitecturajava;
+
+public class HolaMundo11 {
+	public static void main(String[] args) {
+		
+		imprimirHolas("Hola 1", "Saludos 2");
+		
+	}
+	
+	public static void imprimirHolas(String hola1,String hola2) {
+		System.out.println(hola1);
+		System.out.println(hola2);
+	}
+}
