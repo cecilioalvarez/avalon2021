6f4879b683d7677f50263eeb59f3daf1688db864
Ruben <Ruben@DESKTOP-T4NQLMI.home>
2021-07-13 08:59:51 +0200

    nuevas modificaciones

--- 001Basico/src/com/arquitecturajava/HolaMundo9.java
+++ 001Basico/src/com/arquitecturajava/HolaMundo9.java
@@ -6,8 +6,23 @@ public class HolaMundo9 {
 
 	public static void main(String[] args) {
 		
+		System.out.println("Bucle for");
 		for (int i=0; i<10; i++) {
 			System.out.println(i);
 		}
+		
+		System.out.println("Bucle while");
+		int j=0;
+		while(j<10) {
+			System.out.println(j);
+			j++;
+		}
+		
+		System.out.println("Bucle do/while");
+		int k = 0;
+		do {
+			System.out.println(k);
+			k++;
+		}while(k<10);
 	}
 }
