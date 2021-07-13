51972a3ce6bed1dd65641158ea3b79c2350ab698
Gustavo Dea <Gustavo Dea@GusPC>
2021-07-13 11:36:20 +0200

    new

--- 001Basico/src/ejemplo03/Persona.java
+++ 001Basico/src/ejemplo03/Persona.java
@@ -1,15 +1,6 @@
 package ejemplo03;
 
 	public class Persona {
-		String nombre;
-		int edad;
 		
-		boolean estaJubilado () {
-			
-			if(edad>65) {
-				return true;
-			}else {
-				return false;
-			}
-		}
+		
 	}
