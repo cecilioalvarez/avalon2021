f676cdf01884c2c0ac0703e509979227c0824b58
cecilioAlvarez <cmac@arquitecturajava.com>
2021-07-13 09:07:16 +0200

    multiplicar

---
+++ 001Basico/src/com/arquitecturajava/HolaMundo10.java
@@ -0,0 +1,13 @@
+package com.arquitecturajava;
+
+public class HolaMundo10 {
+
+	public static void main(String[] args) {
+		
+		
+		for (int j = 0; j < 10;j++) {
+			System.out.println(j*7);
+		}
+		
+	}
+}
