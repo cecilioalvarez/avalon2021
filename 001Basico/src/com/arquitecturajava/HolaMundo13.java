2ec6a16927436b85d74d175021ce1e981817d08d
miguel <miguel.perez@juanxxiii.net>
2021-07-13 09:44:48 +0200

    HM 13

---
+++ 001Basico/src/com/arquitecturajava/HolaMundo13.java
@@ -0,0 +1,24 @@
+package com.arquitecturajava;
+
+import java.util.Iterator;
+
+public class HolaMundo13 {
+	public static void main(String[] args) {
+		
+		int [] lista= new int[5];
+		lista[0] =4;
+		lista[1] =5;
+		lista[2] =7;
+		lista[3] =8;
+		lista[4] =9;
+		
+		for (int i=0;i<lista.length;i++) {
+			if(lista[i]>=5) {
+				System.out.println("Aprobado: "+lista[i]);
+			}
+			
+		}
+		
+		
+	}
+}
