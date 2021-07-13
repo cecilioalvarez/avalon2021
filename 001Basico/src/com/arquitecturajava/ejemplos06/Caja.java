3af2de2ba313d2f7b5ef6f11c761740be7baeaf9
Francisco Villa <francisco.villa.gonzalez@gmail.com>
2021-07-13 13:44:36 +0200

    caja volumen

--- 001Basico/src/com/arquitecturajava/ejemplos06/Caja.java
+++ 001Basico/src/com/arquitecturajava/ejemplos06/Caja.java
@@ -5,41 +5,46 @@ public class Caja {
 	private double largo;
 	private double alto;
 	private double ancho;
-	
-	
+
 	public Caja(double largo, double alto, double ancho) {
-		
+
 		this.largo = largo;
 		this.alto = alto;
 		this.ancho = ancho;
 	}
+
 	public double getLargo() {
 		return largo;
 	}
+
 	public void setLargo(double largo) {
 		this.largo = largo;
 	}
+
 	public double getAlto() {
 		return alto;
 	}
+
 	public void setAlto(double alto) {
 		this.alto = alto;
 	}
+
 	public double getAncho() {
 		return ancho;
 	}
+
 	public void setAncho(double ancho) {
 		this.ancho = ancho;
 	}
-	
+
 	public double calcularVolumen() {
 		double volumen;
-		volumen=this.getAlto()*this.getAncho()*this.getLargo();
+		volumen = this.getAlto() * this.getAncho() * this.getLargo();
 		return volumen;
-		
-}
+	}
 	
-	
-	
-	
+	public Caja getCajaVolumenHuecoMinimo() {
+		return new Caja (this.getAlto()*1.20,this.getAncho()*1.20,this.getLargo()*1.20);
+	}
+
 }
