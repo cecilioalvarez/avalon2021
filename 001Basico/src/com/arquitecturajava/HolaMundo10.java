4544598ae57ed830cbb05bd8e174b25d53320ce7
Molocale <molocale@gmail.com>
2021-07-13 09:24:44 +0200

    Metodo

---
+++ 001Basico/src/com/arquitecturajava/HolaMundo10.java
@@ -0,0 +1,14 @@
+package com.arquitecturajava;
+
+public class HolaMundo10 {
+	public static void main(String[] args) {
+		
+		imprimirHolas();
+	}
+
+	private static void imprimirHolas() {
+		System.out.println("Hola");
+		
+	}
+
+}
