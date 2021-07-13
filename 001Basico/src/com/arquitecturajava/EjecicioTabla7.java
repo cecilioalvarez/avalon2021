3eb128748144727e43ce8d2606aba40129ab5420
Ruben <Ruben@DESKTOP-T4NQLMI.home>
2021-07-13 09:03:50 +0200

    ejercicio tabla del 7 subido

---
+++ 001Basico/src/com/arquitecturajava/EjecicioTabla7.java
@@ -0,0 +1,16 @@
+package com.arquitecturajava;
+
+
+
+public class EjecicioTabla7 {
+
+	public static void main(String[] args) {
+		
+		int resultado;
+		System.out.println("Tabla del 7");
+		for (int i=0; i<11; i++) {
+			resultado = 7 * i;
+			System.out.println("7 X " + i + " = " + resultado);
+		}
+	}
+}
