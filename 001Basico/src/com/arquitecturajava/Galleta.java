5f3adf13f33cffaf484f5e685f37bc910441c25d
cecilioAlvarez <cmac@arquitecturajava.com>
2021-07-13 10:08:08 +0200

    hola mundo objetos

---
+++ 001Basico/src/com/arquitecturajava/Galleta.java
@@ -0,0 +1,8 @@
+package com.arquitecturajava;
+
+public class Galleta {
+	// propiedades
+	// cada objeto dispone de unas propiedades
+	String sabor;
+	int precio;
+}
---
+++ 001Basico/src/com/arquitecturajava/HolaMundo16.java
@@ -0,0 +1,21 @@
+package com.arquitecturajava;
+
+public class HolaMundo16 {
+
+	public static void main(String[] args) {
+		
+		double[] facturas= new double[3];
+		facturas[0]=100;
+		facturas[1]=200;
+		facturas[2]=300;
+		
+		for (int i=0;i<facturas.length;i++) {
+			
+			System.out.println(facturas[i]*1.21);
+		}
+		
+	}
+	
+	
+
+}
