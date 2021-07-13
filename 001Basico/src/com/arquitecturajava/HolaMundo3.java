f5f11bbf7248d8d2f9c22dcfb974924c444e2d93
Gustavo Dea <Gustavo Dea@GusPC>
2021-07-13 11:17:21 +0200

    new

---
+++ 001Basico/src/com/arquitecturajava/HolaMundo3.java
@@ -0,0 +1,20 @@
+package com.arquitecturajava;
+
+import java.util.Scanner;
+
+public class HolaMundo3 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc= new Scanner(System.in);
+		System.out.println("dame una nota");
+		int numero= sc.nextInt();
+		if (numero>5) {
+			
+			System.out.println("has aprobado");
+		}else {
+			System.out.println("has suspendido");
+		}
+	}
+	
+}
