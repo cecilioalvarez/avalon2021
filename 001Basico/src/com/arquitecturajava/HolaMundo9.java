d434ac9987dae2474172216991506bf4de0c90a0
Molocale <molocale@gmail.com>
2021-07-13 08:54:55 +0200

    Switch, for, while

---
+++ 001Basico/src/com/arquitecturajava/HolaMundo9.java
@@ -0,0 +1,20 @@
+package com.arquitecturajava;
+
+public class HolaMundo9 {
+	public static void main(String[] args) {
+		
+		//bucle o repeticion
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i);
+			
+		}
+		int j=0;
+		while (j<10) {
+			System.out.println(j);
+			j++;
+			
+		}
+	}
+
+}
