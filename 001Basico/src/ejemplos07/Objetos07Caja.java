2bceda9727cce75b12ceb75ec3618634caa975fb
miguel <miguel.perez@juanxxiii.net>
2021-07-13 13:28:31 +0200

    POO07 Caja

---
+++ 001Basico/src/ejemplos07/Objetos07Caja.java
@@ -0,0 +1,13 @@
+package ejemplos07;
+
+public class Objetos07Caja {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Caja c = new Caja(10, 2, 2);
+		
+		System.out.println(c.calculoVolumen());
+	}
+
+}
