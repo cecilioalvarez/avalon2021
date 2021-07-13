0d225470cb4d14f12b92a5bb366d7590f95859cc
cecilioAlvarez <cmac@arquitecturajava.com>
2021-07-13 09:24:25 +0200

    nuevos bucles

--- 001Basico/src/com/arquitecturajava/HolaMundo10.java
+++ 001Basico/src/com/arquitecturajava/HolaMundo10.java
@@ -5,8 +5,15 @@ public class HolaMundo10 {
 	public static void main(String[] args) {
 		
 		
-		for (int j = 0; j < 10;j++) {
+		for (int j = 0; j < 5;j=j+2) {
 			System.out.println(j*7);
+		}
+		int k=0;
+		
+		while (k<5) {
+			
+			System.out.println(k);
+			k=k+1;
 		}
 		
 	}
