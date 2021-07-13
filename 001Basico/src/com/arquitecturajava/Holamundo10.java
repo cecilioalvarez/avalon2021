d1958f5da8aa8bbc4c7e2afa00d6918d0bd6b581
Francisco Villa <francisco.villa.gonzalez@gmail.com>
2021-07-13 09:15:55 +0200

    ejemplo de funcion

---
+++ 001Basico/src/com/arquitecturajava/Holamundo10.java
@@ -0,0 +1,19 @@
+package com.arquitecturajava;
+
+public class Holamundo10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		imprimirHolas();
+		System.out.println("adios");
+		imprimirHolas();
+		
+		public static void imprimirHolas() {
+			System.out.println("hola");
+			System.out.println("hola2");
+		}
+
+	}
+
+}
