a58ed1b81d3da2581eddef1cd752ce26cb55c106
MARIO <MARIO@DESKTOP-A888AC8>
2021-07-13 09:05:54 +0200

    ejercicio bucles

---
+++ 001Basico/src/com/arquitecturajava/tablaMult.java
@@ -0,0 +1,22 @@
+package com.arquitecturajava;
+
+public class tablaMult {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i=0; i<11; i=i+1) {
+			System.out.println(7*i);
+		}
+		int j=0;
+		while (j<10) {
+			System.out.println(7*j);
+			j=j+1;
+		}
+		int k =0;
+		do {
+			System.out.println(7*k);
+			k++;
+		} while (k<10);
+	}
+
+}
