01c09f3c0a7e0a4dd9922ab7cb94039f31e21232
Molocale <molocale@gmail.com>
2021-07-13 13:19:43 +0200

    Caja

---
+++ 001Basico/src/ejercicio06/Caja.java
@@ -0,0 +1,41 @@
+package ejercicio06;
+
+public class Caja {
+	private double alto;
+	private double ancho;
+	private double profundo;
+	public double getAlto() {
+		return alto;
+	}
+	public void setAlto(double alto) {
+		this.alto = alto;
+	}
+	public double getAncho() {
+		return ancho;
+	}
+	public void setAncho(double ancho) {
+		this.ancho = ancho;
+	}
+	public double getProfundo() {
+		return profundo;
+	}
+	public void setProfundo(double profundo) {
+		this.profundo = profundo;
+	}
+	public Caja(double alto, double ancho, double profundo) {
+		this.alto = alto;
+		this.ancho = ancho;
+		this.profundo = profundo;
+	}
+	
+	public Caja() {
+
+	}
+	public double getVolumen(double alto, double ancho, double profundo) {
+		return this.getAncho()*this.getAlto()*this.getProfundo();
+		
+	}
+	
+
+
+}
---
+++ 001Basico/src/ejercicio06/Main.java
@@ -0,0 +1,10 @@
+package ejercicio06;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Caja cajita = new Caja();
+		System.out.println(cajita.getVolumen(15, 10, 3));
+		
+	}
+}
