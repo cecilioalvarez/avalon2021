4a543788a43b92f27db3efda36766f03085b7b33
Ruben <Ruben@DESKTOP-T4NQLMI.home>
2021-07-13 09:08:09 +0200

    ejercicio tabla del 7 mas optimo

--- 001Basico/src/com/arquitecturajava/EjecicioTabla7.java
+++ 001Basico/src/com/arquitecturajava/EjecicioTabla7.java
@@ -6,11 +6,9 @@ public class EjecicioTabla7 {
 
 	public static void main(String[] args) {
 		
-		int resultado;
 		System.out.println("Tabla del 7");
 		for (int i=0; i<11; i++) {
-			resultado = 7 * i;
-			System.out.println("7 X " + i + " = " + resultado);
+			System.out.println("7 X " + i + " = " + (7*i));
 		}
 	}
 }
