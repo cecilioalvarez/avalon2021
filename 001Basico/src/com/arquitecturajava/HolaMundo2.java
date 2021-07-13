551f2a4df58804a40fd0552e1edd8a0997e0d556
Gustavo Dea <Gustavo Dea@GusPC>
2021-07-13 08:37:03 +0200

    ejemplso de ceci

---
+++ 001Basico/src/com/arquitecturajava/HolaMundo2.java
@@ -0,0 +1,14 @@
+package com.arquitecturajava;
+
+public class HolaMundo2 {
+	public static void main(String[]args) {
+		
+		int numero4=4;
+		if (numero4 > 5) {
+			System.out.println("has aprobado");
+		} else {
+			System.out.println();
+		}
+	}
+
+}
