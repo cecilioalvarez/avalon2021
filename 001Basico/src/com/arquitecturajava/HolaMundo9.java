327713a90d7d1b472cd0ac359454a9a1a2a63d98
miguel <miguel.perez@juanxxiii.net>
2021-07-13 08:59:05 +0200

    HM8 y 9

---
+++ 001Basico/src/com/arquitecturajava/HolaMundo9.java
@@ -0,0 +1,24 @@
+package com.arquitecturajava;
+
+public class HolaMundo9 {
+	public static void main(String[] args) {
+		
+		for(int i=0;i<10;i++) {
+			System.out.println(i);
+		}
+		
+		int j=00;
+		
+		while(j<10) {
+			System.out.println(j);
+			j++;
+		}
+		
+		int k=0;
+		do {
+			System.out.println(k);
+			k++;
+		}while(k<10);
+		
+	}
+}
