d210477edc92a126848e3e92964532d1b0f45c4e
cecilioAlvarez <cmac@arquitecturajava.com>
2021-07-13 09:50:25 +0200

    funcion suma

---
+++ 001Basico/src/com/arquitecturajava/HolaMundo15.java
@@ -0,0 +1,17 @@
+package com.arquitecturajava;
+
+public class HolaMundo15 {
+
+	public static void main(String[] args) {
+		
+		//invocacion del funcion con el resultado
+		double resultado=suma(2,2);
+		System.out.println(resultado);
+	}
+	// declaracion de la funcion
+	public static double suma (int a ,int b) {
+		
+		return a+b;
+	}
+
+}
