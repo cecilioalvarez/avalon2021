81a96d423805916c068068f149cc39700ca94783
Ruben <Ruben@DESKTOP-T4NQLMI.home>
2021-07-13 08:51:10 +0200

    nuevas clases

---
+++ 001Basico/src/com/arquitecturajava/HolaMundo8.java
@@ -0,0 +1,11 @@
+package com.arquitecturajava;
+
+
+
+public class HolaMundo8 {
+
+	public static void main(String[] args) {
+		
+		
+	}
+}
---
+++ 001Basico/src/com/arquitecturajava/HolaMundo9.java
@@ -0,0 +1,13 @@
+package com.arquitecturajava;
+
+
+
+public class HolaMundo9 {
+
+	public static void main(String[] args) {
+		
+		for (int i=0; i<10; i++) {
+			System.out.println(i);
+		}
+	}
+}
