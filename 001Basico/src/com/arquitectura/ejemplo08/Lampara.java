b545d4c47aa662812289485d561ff1c1b1f3a33f
Francisco Villa <francisco.villa.gonzalez@gmail.com>
2021-07-13 14:13:35 +0200

    Lamparas con sobrecarga de constructores

--- 001Basico/src/com/arquitectura/ejemplo08/Lampara.java
+++ 001Basico/src/com/arquitectura/ejemplo08/Lampara.java
@@ -2,10 +2,29 @@ package com.arquitectura.ejemplo08;
 
 public class Lampara {
 	private String marca;
-
+	private int lumenex;
+	
+	public Lampara(String marca,int lumenex) {
+		this.setMarca(marca);
+		this.setLumenex(lumenex);
+	}
 	public Lampara(String marca) {
 		this.setMarca(marca);
+		this.setLumenex(10);
 	}
+	public Lampara() {
+		this.setMarca("m3");
+		this.setLumenex(8);
+	}
+	
+	public int getLumenex() {
+		return lumenex;
+	}
+
+	public void setLumenex(int lumenex) {
+		this.lumenex = lumenex;
+	}
+
 	public String getMarca() {
 		return marca;
 	}
--- 001Basico/src/com/arquitectura/ejemplo08/Objetos8.java
+++ 001Basico/src/com/arquitectura/ejemplo08/Objetos8.java
@@ -4,7 +4,7 @@ public class Objetos8 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Lampara lampara=new Lampara("m1");
+		Lampara lampara=new Lampara("m1",5);
 		lampara.encender();
 		lampara.encender(3);
 	}
