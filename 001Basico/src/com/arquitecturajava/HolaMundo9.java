dff01d8b64dfab7ea228539ded558542a937194e
Gustavo Dea <Gustavo Dea@GusPC>
2021-07-13 08:55:57 +0200

    Gustavo

---
+++ 001Basico/src/com/arquitecturajava/HolaMundo9.java
@@ -0,0 +1,18 @@
+package com.arquitecturajava;
+
+public class HolaMundo9 {
+	public static void main(String[] args) {
+
+	//bucle o repeticion
+	for (int i=0;i<10;i=i+1) {
+		
+		System.out.println(i);
+	}
+	int j=0;
+	
+	while (j<10) {
+		System.out.println(j);
+		j++;
+	}
+}
+}
