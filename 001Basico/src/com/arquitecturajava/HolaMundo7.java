39f476809662f62df142bb976ec43a0269391ffa
Francisco Villa <francisco.villa.gonzalez@gmail.com>
2021-07-13 09:15:37 +0200

    ejemplo de funcion

--- 001Basico/src/com/arquitecturajava/HolaMundo7.java
+++ 001Basico/src/com/arquitecturajava/HolaMundo7.java
@@ -1,16 +1,14 @@
 package com.arquitecturajava;
 
-import java.util.Scanner;
-
 public class HolaMundo7 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for(int i=0;i<10;i++) {
-			
-			System.out.println("7 x "+ i + " = "+ 7*i);
+		for (int i = 0; i < 10; i++) {
+
+			System.out.println("7 x " + i + " = " + 7 * i);
 		}
-		
+
 	}
 
 }
