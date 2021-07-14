364d8379ce8cdcc37f9af10581623be95cf6003e
miguel <miguel.perez@juanxxiii.net>
2021-07-14 10:40:16 +0200

    POO13 Notas

--- 001Basico/src/ejemplos13POO/Objetos13.java
+++ 001Basico/src/ejemplos13POO/Objetos13.java
@@ -12,10 +12,23 @@ public class Objetos13 {
 		notas.add(new Nota("ingles", 2));
 		notas.add(new Nota("fisica", 10));
 		
+		int suma=0;
+		Nota notaMax=new Nota("",0);
+		
 		for (Nota nota : notas) {
+			
+			if (nota.getValor()>notaMax.getValor()) {
+				notaMax=nota;
+			}
+			
 			System.out.println(nota.getValor());
-		}
+			suma+=nota.getValor();
+		};
 
+		System.out.println("La suma de Notas: "+suma);
+		System.out.println("La Media de Notas: "+suma/notas.size());
+		System.out.println("La Nota Maxima: "+notaMax.getValor());
+		
 	}
 
 }
