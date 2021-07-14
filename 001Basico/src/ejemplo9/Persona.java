08693aec6585ad01cef3970312053e6870f2c942
Gustavo Dea <Gustavo Dea@GusPC>
2021-07-14 09:04:53 +0200

    nuevo

--- 001Basico/src/ejemplo9/Persona.java
+++ 001Basico/src/ejemplo9/Persona.java
@@ -1,5 +1,39 @@
 package ejemplo9;
 
 public class Persona {
-
+	private String nombre;
+	private int edad;
+	
+	//variable que no es una cadena 
+	//ni es un tipo basico
+	//
+	private int Telefono
+	
+	
+	public int getTelefono() {
+		return Telefono;
+	}
+	public void setTelefono(int telefono) {
+		Telefono = telefono;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public Persona(String nombre, int edad, int telefono) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+		Telefono = telefono;
+	}
+	
+	
 }
--- 001Basico/src/ejemplo9/Telefono.java
+++ 001Basico/src/ejemplo9/Telefono.java
@@ -1,5 +1,28 @@
 package ejemplo9;
 
 public class Telefono {
-
+	
+	private int numero;
+	private String marca;
+	
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	
+	public Telefono(int numero, String marca) {
+		super();
+		this.numero = numero;
+		this.marca = marca;
+	}
+	
+	
 }
