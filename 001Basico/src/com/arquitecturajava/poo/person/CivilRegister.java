d9a94e27e522afe9ee7d58c9dc81af8ddffa5da5
Germán Zunzunegui <gerzunzu@gmail.com>
2021-07-14 10:01:21 +0200

    N - ArrayList en el paquete Persona.

--- 001Basico/src/com/arquitecturajava/poo/person/CivilRegister.java
+++ 001Basico/src/com/arquitecturajava/poo/person/CivilRegister.java
@@ -1,39 +1,42 @@
 package com.arquitecturajava.poo.person;
 
+import java.util.ArrayList;
+
 class CivilRegister {
     
-    private Person person;
+    private final ArrayList<Person> PEOPLE;
 
-    private CivilRegister(Person person) {
-        this.setPerson(person);
+    private CivilRegister() {
+        this.PEOPLE = new ArrayList<>();
     }
 
-    private Person getPerson() {
-        return person;
+    private ArrayList getPeople() {
+        return this.PEOPLE;
     }
 
-    private void setPerson(Person person) {
-        this.person = person;
+    private void addPerson(Person person) {
+        this.PEOPLE.add(person);
     }
 
-    private void showPersonData() {
-        System.out.println(this.person.toString());
+    private void showPeopleData() {
+        for (Person person : this.PEOPLE) {
+            System.out.println(person.toString());
+        }
     }
 
     public static void main(String[] args) {
+        CivilRegister civilRegister = new CivilRegister();
+        
         Person person = new Person();
         person.setName("Manuel");
         person.setAge(17);
         person.setPet(new Pet("Calcetines", new Illness()));
         person.setGender(Gender.MALE);
         
-        CivilRegister civilRegister = new CivilRegister(person);
-        civilRegister.showPersonData();
+        civilRegister.addPerson(person);
+        civilRegister.addPerson(new Person("Francisca", 89, new Phone(722597102, "Motorola"), new Pet("Toby", new Illness("Fibrosis", Extent.ACUTE)), Gender.FEMALE));
+        civilRegister.addPerson(new Person("Andrea", 52, new Phone(654880307, "Sony"), null, Gender.OTHER));
         
-        civilRegister.setPerson(new Person("Francisca", 89, new Phone(722597102, "Motorola"), new Pet("Toby", new Illness("Fibrosis", Extent.ACUTE)), Gender.FEMALE));
-        civilRegister.showPersonData();
-        
-        civilRegister.setPerson(new Person("Andrea", 52, new Phone(654880307, "Sony"), null, Gender.OTHER));
-        civilRegister.showPersonData();
+        civilRegister.showPeopleData();
     }
 }
