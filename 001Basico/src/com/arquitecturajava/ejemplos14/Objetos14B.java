1bf5cd89a242c2f49c0bac3894f91b77f37ad8a2
cecilioAlvarez <cmac@arquitecturajava.com>
2021-07-14 14:08:58 +0200

    mi version de rectangulo instancia

---
+++ 001Basico/src/com/arquitecturajava/ejemplos14/Objetos14B.java
@@ -0,0 +1,12 @@
+package com.arquitecturajava.ejemplos14;
+
+public class Objetos14B {
+public static void main(String[] args) {
+	
+	
+	Rectangulo r1= new Rectangulo (2,2);
+	
+	System.out.println(r1.area());
+	System.out.println(r1.perimetro());
+}
+}
