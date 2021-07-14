980dd796aeb479bfa60d2be9ae07504b7aeea340
cecilioAlvarez <cmac@arquitecturajava.com>
2021-07-14 10:37:13 +0200

    suma y media

--- 001Basico/src/com/arquitecturajava/ejemplos13/Objetos13.java
+++ 001Basico/src/com/arquitecturajava/ejemplos13/Objetos13.java
@@ -5,18 +5,30 @@ import java.util.ArrayList;
 public class Objetos13 {
 
 	public static void main(String[] args) {
-		
-		ArrayList<Nota> notas= new ArrayList<Nota>(); 
-		notas.add(new Nota("mates",5));
-		notas.add(new Nota("lengua",6));
-		notas.add(new Nota("ingles",2));
-		notas.add(new Nota("fisica",10));
-		
+
+		ArrayList<Nota> notas = new ArrayList<Nota>();
+		notas.add(new Nota("mates", 5));
+		notas.add(new Nota("lengua", 6));
+		notas.add(new Nota("ingles", 2));
+		notas.add(new Nota("fisica", 10));
+
+		Nota mayor = new Nota("lengua", 0);
+		double sumaNotas=0;
 		for (Nota nota : notas) {
-			
-			System.out.println(nota.getValor());
+
+			if (mayor.getValor() < nota.getValor()) {
+				mayor = nota;
+			}
 		}
-		
+		System.out.println(mayor.getValor());
+
+		for (Nota nota : notas) {
+
+			//sumaNotas=sumaNotas+nota.getValor();
+			sumaNotas+=nota.getValor();
+		}
+
+		System.out.println(sumaNotas/notas.size());
 	}
-	
+
 }
