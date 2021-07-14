f95573ed930174aa67e915bb783407a8a86b6dcc
miguel <miguel.perez@juanxxiii.net>
2021-07-14 09:19:26 +0200

    POO11

--- 001Basico/src/ejemplos11POO/Objeto11.java
+++ 001Basico/src/ejemplos11POO/Objeto11.java
@@ -3,7 +3,6 @@ package ejemplos11POO;
 public class Objeto11 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		Persona p1 = new Persona("Pedro", 23, new Mascota("Kira"));
 		
