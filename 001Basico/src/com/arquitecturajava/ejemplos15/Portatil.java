4fb4a963af2d0742ecfba4a97228f37281464f2d
cecilioAlvarez <cmac@arquitecturajava.com>
2021-07-15 09:05:21 +0200

    ejemplo inicial herencia

---
+++ 001Basico/src/com/arquitecturajava/ejemplos15/Portatil.java
@@ -0,0 +1,14 @@
+package com.arquitecturajava.ejemplos15;
+
+public class Portatil extends Ordenador {
+
+	private int bateria;
+
+	public Portatil(String marca, double precio, int bateria) {
+		super(marca, precio);
+		this.bateria=bateria;
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}
