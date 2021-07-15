65f4353c9ab6a9571971dd68b84f18a4a8511818
cecilioAlvarez <cmac@arquitecturajava.com>
2021-07-15 09:09:54 +0200

    nuevo

--- 001Basico/src/com/arquitecturajava/ejemplos15/Portatil.java
+++ 001Basico/src/com/arquitecturajava/ejemplos15/Portatil.java
@@ -7,6 +7,7 @@ public class Portatil extends Ordenador {
 	public Portatil(String marca, double precio, int bateria) {
 		super(marca, precio);
 		this.bateria=bateria;
+		
 		// TODO Auto-generated constructor stub
 	}
 	
