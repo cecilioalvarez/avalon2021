ab7efb77bae673c4a8e5387be3d5119e21986f09
cecilioAlvarez <cmac@arquitecturajava.com>
2021-07-15 11:12:47 +0200

    nuevo ejemplo

---
+++ 001Basico/src/ejemplo16/ManejoLista2.java
@@ -0,0 +1,19 @@
+package ejemplo16;
+
+import java.util.ArrayList;
+
+public class ManejoLista2 {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> numeros= new ArrayList<Integer>();
+		numeros.add(1);
+		numeros.add(2);
+		int total=0;
+		for(int numero: numeros) {
+			
+			total+=numero;
+		}
+		System.out.println(total);
+	}
+}
