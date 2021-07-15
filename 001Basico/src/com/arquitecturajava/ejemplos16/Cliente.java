665a025905f7270f0e0db561bab8c29feb49c913
cecilioAlvarez <cmac@arquitecturajava.com>
2021-07-15 11:15:41 +0200

    clase cliente

---
+++ 001Basico/src/com/arquitecturajava/ejemplos16/Cliente.java
@@ -0,0 +1,39 @@
+package com.arquitecturajava.ejemplos16;
+
+import java.util.ArrayList;
+
+public class Cliente {
+
+	private String nombre;
+	private ArrayList<Compra> compras= new ArrayList<Compra>();
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public ArrayList<Compra> getCompras() {
+		return compras;
+	}
+	public void setCompras(ArrayList<Compra> compras) {
+		this.compras = compras;
+	}
+	
+	public void addCompra (Compra c) {
+		compras.add(c);
+	}
+	
+	public double  getImporteTotalCompras() {
+		
+		double total=0;
+		for (Compra c : compras) {
+			
+			total+=c.getImporte();
+			
+			
+		}
+		return total;
+	}
+	
+	
+}
