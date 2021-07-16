1e5cb14d90a1127328300cac8c9b2b604e2ee627
cecilioAlvarez <cmac@arquitecturajava.com>
2021-07-16 14:16:55 +0200

    factory revision

--- 001Basico/src/com/arquitecturajava/ejemplos23/Objeto23.java
+++ 001Basico/src/com/arquitecturajava/ejemplos23/Objeto23.java
@@ -4,11 +4,15 @@ public class Objeto23 {
 public static void main(String[] args) {
 	
 	Documento d= new Documento(1,"hola esto es texto",1);
-	GestorDocumental gd= new GestorDocumentalWord();
+	
+	//Patron de factoria para crear los diferentes gestores de documentos
+	// el que usa el programa no sabe cuantos tipos de gestores exdisten
+	
+	GestorDocumental gd= GestorFactory.getGestor("wordpress");
 	gd.salvar(d);
 	gd.versionar(d);
 	
-	GestorDocumental gd1= new GestorDocumentalWordpress();
+	GestorDocumental gd1= GestorFactory.getGestor("word");
 	gd1.salvar(d);
 	gd1.versionar(d);
 	
