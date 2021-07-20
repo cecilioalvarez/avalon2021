41be0ecbb5c0eddba4c6f7a558337656c8d56ac1
miguel <miguel.perez@juanxxiii.net>
2021-07-20 21:00:07 +0200

    Solucion con métodos y una clase Libro Ej32

--- 001Basico/src/ejemplos32_SQL2_Ejercicio/SQL2_EjercicioInsercion2_Libro_SolucionCecilio.java
+++ 001Basico/src/ejemplos32_SQL2_Ejercicio/SQL2_EjercicioInsercion2_Libro_SolucionCecilio.java
@@ -21,36 +21,23 @@ public class SQL2_EjercicioInsercion2_Libro_SolucionCecilio {
 		Scanner sc = new Scanner(System.in);
 		List<Libro> myListaLibros = new ArrayList<Libro>();
 		
-		//String queryInsert= "INSERT INTO libro(isbn, titulo, autor) VALUES(?,?,?)";
-		//String queryDelete= "DELETE FROM libro WHERE isbn=?";
-		//String queryUpdate= "UPDATE libro set autor='Pedro' WHERE isbn=?";
-		try(Connection conn = DriverManager.getConnection(DB_URL,USER,PASS);
-				Statement sentencia = conn.createStatement();) {
+		Libro l1 = new Libro("A3", "aria", "Libro de Maria");
+		
+		//addBook(l1);
 			
-			conn.setAutoCommit(false);
-			try(PreparedStatement prepSentencia = conn.prepareStatement(queryInsert);){
-				//PreparedStatement
-				/*prepSentencia.setString(1, "A1");
-				prepSentencia.setString(2, "Libro de David");
-				prepSentencia.setString(3, "david");
-				
-				prepSentencia.execute();*/
-				
-			}catch (Exception ex) {
-				ex.printStackTrace();
-				conn.rollback();
-			}
-			conn.commit();
-			
-			myListaLibros = buscarTodosLibros();
-			for (Libro libro : myListaLibros) {
-				System.out.println(libro.getIsbn()+"-"+libro.getAutor()+"-"+libro.getTitulo());
-			}
-			
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		myListaLibros = buscarTodosLibros();
+		for (Libro libro : myListaLibros) {
+			System.out.println(libro.getIsbn()+"-"+libro.getAutor()+"-"+libro.getTitulo());
 		}
+		
+		modifyBook("A3", "María");
+		
+		System.out.println("------------------------");
+		myListaLibros = buscarTodosLibros();
+		for (Libro libro : myListaLibros) {
+			System.out.println(libro.getIsbn()+"-"+libro.getAutor()+"-"+libro.getTitulo());
+		}
+		
 		
 
 	}
@@ -61,7 +48,7 @@ public class SQL2_EjercicioInsercion2_Libro_SolucionCecilio {
 		try(Connection conn = DriverManager.getConnection(DB_URL,USER,PASS);
 				Statement sentencia = conn.createStatement();) {
 			//Para Resultados de SELECT
-			ResultSet rs = sentencia.executeQuery(QUERY);
+			ResultSet rs = sentencia.executeQuery(querySelect);
 			
 			while (rs.next()) {
 				Libro l = new Libro(rs.getString("isbn"), rs.getString("autor"), rs.getString("titulo"));
@@ -74,8 +61,53 @@ public class SQL2_EjercicioInsercion2_Libro_SolucionCecilio {
 		return listaLibros;
 	}
 	
-	public static void modifyBook() {
-		
+	public static void modifyBook(String isbn,String autor) {
+		String queryUpdate = "UPDATE libro set autor=? WHERE isbn=?";
+		try(Connection conn = DriverManager.getConnection(DB_URL,USER,PASS);){
+			conn.setAutoCommit(false);
+			try(PreparedStatement prepSentencia = conn.prepareStatement(queryUpdate);){
+				//PreparedStatement
+			
+				prepSentencia.setString(1, autor);
+				prepSentencia.setString(2, isbn);
+			
+				prepSentencia.execute();
+			}catch (Exception ex) {
+				ex.printStackTrace();
+				conn.rollback();
+			}
+			
+			conn.commit();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void addBook(Libro libro) {
+		String queryInsert = "INSERT INTO libro(isbn, titulo, autor) VALUES(?,?,?)";
+		try(Connection conn = DriverManager.getConnection(DB_URL,USER,PASS);){
+			conn.setAutoCommit(false);
+			try(PreparedStatement prepSentencia = conn.prepareStatement(queryInsert);){
+				//PreparedStatement
+			
+				prepSentencia.setString(1, libro.getIsbn());
+				prepSentencia.setString(2, libro.getTitulo());
+				prepSentencia.setString(3, libro.getAutor());
+			
+				prepSentencia.execute();
+			}catch (Exception ex) {
+				ex.printStackTrace();
+				conn.rollback();
+			}
+			
+			conn.commit();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
