1139a1d90a57b09b404c94fa5522719525f79f6c
cecilioAlvarez <cmac@arquitecturajava.com>
2021-07-20 12:42:25 +0200

    lectura con scanner

--- 001Basico/src/com/arquitecturajava/ejemplos34/SQL4.java
+++ 001Basico/src/com/arquitecturajava/ejemplos34/SQL4.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class SQL4 {
 
@@ -14,12 +15,20 @@ public class SQL4 {
 	//static final String CONSULTA = "insert into Personas (dni,nombre,edad) values (10,'angel',20)";
 
 	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int dni=0;
+		String nombre=null;
+		int edad=0;
+		System.out.println("por favor introduce un dni");
+		dni=sc.nextInt();
+		System.out.println("por favor introduce un nombre");
+		sc.nextLine();
+		nombre=sc.nextLine();
+		System.out.println("por favor introduce la edad");
+		edad=sc.nextInt();
 
 		try (Connection conn = DriverManager.getConnection(URL, USUARIO, CLAVE);
 				Statement sentencia = conn.createStatement();) {
-			int dni=0;
-			String nombre=null;
-			int edad=0;
 			String CONSULTA= "insert into Personas (dni,nombre,edad) values ("+ dni +",'"+ nombre+"',"+edad+")";
 			sentencia.executeUpdate(CONSULTA);
 
