3761a3c9e2fe293c51ce6e601470822b08c10dcb
cecilioAlvarez <cmac@arquitecturajava.com>
2021-07-21 09:42:56 +0200

    database helper

---
+++ 001Basico/src/com/arquitecturajava/ejemplos35/Libro.java
@@ -0,0 +1,62 @@
+package com.arquitecturajava.ejemplos35;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+
+public class Libro {
+
+	private String isbn;
+	private String titulo;
+	private String autor;
+	private DataBaseHelper helper;
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public Libro(String isbn, String titulo, String autor,DataBaseHelper helper) {
+		super();
+		this.isbn = isbn;
+		this.titulo = titulo;
+		this.autor = autor;
+		this.helper=helper;
+	}
+	
+	public   void  insertar () {
+		
+		 final String CONSULTA = "insert into Libros (isbn,titulo,autor) values (?,?,?)";
+			try (Connection conn = DriverManager.getConnection(helper.getUrl(),helper.getUser(), helper.getPassword());
+					PreparedStatement sentencia = conn.prepareStatement(CONSULTA);
+					) {
+
+					sentencia.setString(1, this.getIsbn());
+					sentencia.setString(2, this.getTitulo());
+					sentencia.setString(3, this.getAutor());
+					sentencia.execute();
+				
+
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		 
+	}
+	
+}
