31dc3bb2c6f157a0e9c7cf2830aa9ae3f738482a
cecilioAlvarez <cmac@arquitecturajava.com>
2021-07-22 12:13:51 +0200

    creacion de la clase capitulo

---
+++ 001Basico/src/com/arquitecturajava/ejemplos37/Capitulo.java
@@ -0,0 +1,49 @@
+package com.arquitecturajava.ejemplos37;
+
+public class Capitulo {
+
+	private String titulo;
+	private int paginas;
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public int getPaginas() {
+		return paginas;
+	}
+	public void setPaginas(int paginas) {
+		this.paginas = paginas;
+	}
+	public Capitulo(String titulo, int paginas) {
+		super();
+		this.titulo = titulo;
+		this.paginas = paginas;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((titulo == null) ? 0 : titulo.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Capitulo other = (Capitulo) obj;
+		if (titulo == null) {
+			if (other.titulo != null)
+				return false;
+		} else if (!titulo.equals(other.titulo))
+			return false;
+		return true;
+	}
+	
+}
