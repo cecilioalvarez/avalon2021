daced54fb9e5b42d03fbfe66ea8e776ee93c1139
cecilioAlvarez <cmac@arquitecturajava.com>
2021-08-12 10:05:58 +0200

    comparadores

--- 001Basico/src/com/arquitecturajava/ejemplos40/Principal.java
+++ 001Basico/src/com/arquitecturajava/ejemplos40/Principal.java
@@ -1,9 +1,31 @@
 package com.arquitecturajava.ejemplos40;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		List<Persona> lista= new ArrayList<Persona>();
+	
+		
+		
+		lista.add(new Persona("pedro","gomez",20));
+		lista.add(new Persona("maria","blanco",30));
+		lista.add(new Persona("gema","alvarez",10));
+		
+		Collections.sort(lista);
+		
+		
+		for (Persona p:lista) {
+			
+			System.out.println(p);
+		}
+		
+		
 
 	}
 
